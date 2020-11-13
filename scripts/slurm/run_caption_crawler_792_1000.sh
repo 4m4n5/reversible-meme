878e025544836bf2c1fc8e305a5badb62527cf82
Aman Shrivastava <aman.srivastava999@gmail.com>
2020-11-12 20:35:56 -0500

    captions crawled

---
+++ scripts/slurm/run_caption_crawler_792_1000.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#SBATCH --job-name="cp792_1000"
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=as3ek@virginia.edu
+#
+#SBATCH --error="/u/as3ek/github/reversible-meme/scripts/logs/crawler_792_1000.err"
+#SBATCH --output="/u/as3ek/github/reversible-meme/scripts/logs/crawler_792_1000.output"
+
+module load anaconda3
+
+python -u /u/as3ek/github/reversible-meme/scripts/caption_crawler.py --sm 792 --em 1000
+
